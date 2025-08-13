ca3eedbe6e37710cc96874d9bb6fc4b87605c16e
Vladislav <vladislav_vorobev04@mail.ru>
2025-08-13 16:36:31 +0300

    fix views for base page

--- main/views.py
+++ main/views.py
@@ -8,60 +8,61 @@ from .models import *
 
 class IndexView(TemplateView):
     template_name = 'main/base.html'
-    
+
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context("categories") = Category.objects.all()
+        context['categories'] = Category.objects.all()
         context['current_category'] = None
-        return
-        
-        
+        return context
+    
+
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
         if request.headers.get('HX-Request'):
-            return TemplateResponse(request, 'main/home_content.html')
+            return TemplateResponse(request, 'main/home_content.html', context)
         return TemplateResponse(request, self.template_name, context)
     
 
 class CatalogView(TemplateView):
     template = 'main/base.html'
-    
+
     FILTER_MAPPING = {
         'color': lambda queryset, value: queryset.filter(color__iexact=value),
         'min_price': lambda queryset, value: queryset.filter(price_gte=value),
         'max_price': lambda queryset, value: queryset.filter(price_lte=value),
         'size': lambda queryset, value: queryset.filter(product_size__size__name=value),
     }
-    
-    
+
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         category_slug = kwargs.get('category_slug')
         categories = Category.objects.all()
         products = Product.objects.all().order_by('-created_at')
         current_category = None
-        
+
         if category_slug:
             current_category = get_object_or_404(Category, slug=category_slug)
             products = products.filter(category=current_category)
-            
+
         query = self.request.GET.get('q')
         if query:
             products = products.filter(
-                Q(name__incontains=query) | Q(description__incontains=query)
+                Q(name__icontains=query) | Q(description__icontains=query)
             )
-            
+
         filter_params = {}
         for param, filter_func in self.FILTER_MAPPING.items():
-            value = self.request.GET.get(param)    
+            value = self.request.GET.get(param)
             if value:
                 products = filter_func(products, value)
                 filter_params[param] = value
             else:
                 filter_params[param] = ''
-                
+
         filter_params['q'] = query or ''
-        
+
         context.update({
             'categories': categories,
             'products': products,
@@ -70,16 +71,15 @@ class CatalogView(TemplateView):
             'sizes': Size.objects.all(),
             'search_query': query or ''
         })
-        
-        
+
         if self.request.GET.get('show_search') == 'true':
             context['show_search'] = True
         elif self.request.GET.get('reset_search') == 'true':
             context['reset_search'] = True
         
-        return
-
+        return context
     
+
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
         if request.headers.get('HX-Request'):
@@ -91,23 +91,25 @@ class CatalogView(TemplateView):
             return TemplateResponse(request, template, context)
         return TemplateResponse(request, self.template_name, context)
     
-    
+
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'main/base.html'
     slug_field = 'slug'
     slug_url_kwarg = 'slug'
-    
+
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         product = self.get_object()
         context['categories'] = Category.objects.all()
         context['related_products'] = Product.objects.filter(
-            caregory = product.category
+            category=product.category
         ).exclude(id=product.id)[:4]
         context['current_category'] = product.category.slug
-        return
+        return context
     
+
     def get(self, request, *args, **kwargs):
         self.object = self.get_object()
         context = self.get_context_data(**kwargs)
